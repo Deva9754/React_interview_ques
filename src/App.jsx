8e07ee9bd85ca3880872751a4c2cf646b308bd54
Deva9754 <deva.sahu2001@gmail.com>
2025-01-17 22:05:06 +0530

    add: tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import './index.css';  
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
 import AdminPage from './components/AdminPage';
 import HomePage from './components/HomePage';
-import Layout from './components/Layout';  
 import { Provider } from 'react-redux';
 import store from '../src/store/store';
+import Quiz from './components/Quiz';
+import Pagination from './components/Pagination';
 
 const App = () => {
   return (
@@ -14,10 +16,12 @@ const App = () => {
       <Router>
         <Routes>
           {/* Wrap pages with Layout component to include the header and home button */}
-          <Route path="/" element={<Layout><HomePage /></Layout>} />
-          <Route path="/signup" element={<Layout><SignUpPage /></Layout>} />
-          <Route path="/login" element={<Layout><LoginPage /></Layout>} />
-          <Route path="/admin" element={<Layout><AdminPage /></Layout>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/quiz" element={<Quiz/>}/>
+          <Route path ="/pagination" element={<Pagination/>}/>
         </Routes>
       </Router>
     </Provider>
